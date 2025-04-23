f5b615f696ec4ccd5cc9c26b49d3d83b4797c384
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-04-23 20:48:55 +0330

    Refactor post and user model definitions for improved readability and consistency

--- models/posts-model.js
+++ models/posts-model.js
@@ -1,22 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-    const PostModel = sequelize.define('posts', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        content: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        userId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'users',
-                key: 'id'
-            },
-            allowNull: false
-
-        }
-    })
-    return PostModel;
-}
+  const PostModel = sequelize.define("posts", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      allowNull: false,
+    },
+  });
+  return PostModel;
+};
--- models/users-model.js
+++ models/users-model.js
@@ -1,19 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-    const UserModel = sequelize.define('users', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-
-        }
-    })
-    return UserModel;
-}
+  const UserModel = sequelize.define("users", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return UserModel;
+};
