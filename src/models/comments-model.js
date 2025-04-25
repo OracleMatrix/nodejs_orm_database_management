9a92f6a5838808b09565559f96da813821f14cb7
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-04-25 13:58:08 +0330

    Refactor CommentsModel to use class syntax, enhancing structure and maintainability.

--- src/models/comments-model.js
+++ src/models/comments-model.js
@@ -1,29 +1,53 @@
-module.exports = (sequelize, DataTypes) => {
-  const CommentModel = sequelize.define("comments", {
-    content: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "users",
-        key: "id",
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  class CommentsModel extends Model {
+    static associate(models) {
+      CommentsModel.belongsTo(models.users, {
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
+      CommentsModel.belongsTo(models.posts, {
+        foreignKey: "postId",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
+    }
+  }
+
+  CommentsModel.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        allowNull: false,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
       },
-      allowNull: false,
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-    },
-    postId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "posts",
-        key: "id",
+      postId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "posts",
+          key: "id",
+        },
+        allowNull: false,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
       },
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-      allowNull: false,
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
-  return CommentModel;
+    {
+      sequelize,
+      modelName: "comments",
+    }
+  );
+
+  return CommentsModel;
 };
